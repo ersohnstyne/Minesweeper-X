36807b75a532e4334b62a36d16c0fc715e85f1d5
PennyGames <33354707+ersohnstyne@users.noreply.github.com>
2018-11-15 10:19:38 +0100

    Extras übersehen

--- src/com/ersohn/windows10/minesweeper/resources/SplashGame.java
+++ src/com/ersohn/windows10/minesweeper/resources/SplashGame.java
@@ -1,20 +1,3 @@
-/*
- * SplashScreen.java
- *
- * Copyright (c) 2003-2017 Sunnyball authors
- * See authors.txt for details.
- *
- * Sunnyball is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- */
-
 package com.ersohn.windows10.minesweeper.resources;
 
 import java.awt.*;
@@ -31,9 +14,6 @@ public class SplashGame extends Frame implements ActionListener {
   }
   
   public SplashGame() throws NullPointerException {
-    super("SplashScreen game");
-    setSize(640, 400);
-    setLayout(new BorderLayout());
     final SplashScreen splash = SplashScreen.getSplashScreen();
     
     if (splash == null) {
